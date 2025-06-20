48d500b9b249dd248777629fe16f083cd9459934
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-06-20 10:43:24 -0500

    add archives

---
+++ galleryPhotos/IMAGES.js
@@ -0,0 +1,18 @@
+export const IMAGES = [
+  {
+    id: 1,
+    titulo: "Montaña",
+    url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+  },
+  {
+    id: 2,
+    titulo: "Playa",
+    url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+  },
+  {
+    id: 3,
+    titulo: "Bosque",
+    url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+  },
+  
+];
